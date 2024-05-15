6521560c2c60b5c253b4c195edb73d1d28d2dd0a
MeghnaGupta436 <157966037+MeghnaGupta436@users.noreply.github.com>
2024-05-15 12:56:40 +0530

    Update GetSingle.java

--- TestAutomationAssignment_2/src/test/java/CRUD/StepDefinations/GetSingle.java
+++ TestAutomationAssignment_2/src/test/java/CRUD/StepDefinations/GetSingle.java
@@ -5,28 +5,30 @@ import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class GetSingle {
     private Response response;
 
 //to get the single employee details using get request
-    @When("^Get request for single Employee$")
+    @When("Get request for single user")
     public void getSingleRequest(){
-        response= given().baseUri("https://dummy.restapiexample.com/")
-                .basePath("api/v1/employee")
-                .pathParams("data.id","1")
+        response= given().baseUri("https://reqres.in/")
+                .basePath("api/users/2")
                 .when().get()
                 .then()
                 .extract()
                 .response();
     }
     //to validate the response of request
-    @Then("^Single employee data is shown$")
+    @Then("Single user data is shown")
     public void getSingleResponse(){
          response.then().statusCode(200);
         //body should contain some data , should not be null
          response.then().body("data",notNullValue());
+         response.then().body("data.id",equalTo(2));
+         response.then().body("data.first_name",equalTo("Janet"));
 
     }
 }
